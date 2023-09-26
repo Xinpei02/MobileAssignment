5450e96937490b56dd6eb725446457dd5f7d380b
Xinpei02 <tanxp-wp20@student.tarc.edu.my>
2023-09-27 01:05:15 +0800

    commit

--- app/src/main/java/Admin/AdminPayment.kt
+++ app/src/main/java/Admin/AdminPayment.kt
@@ -7,28 +7,28 @@ import android.os.Bundle
 import com.example.mobileassignment.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class AdminMainActivity : AppCompatActivity() {
+class AdminPayment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_adminmain)
+        setContentView(R.layout.activity_admin_payment)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.admin_navigation)
-        bottomNavigationView.selectedItemId = R.id.admin_home
+        bottomNavigationView.selectedItemId = R.id.admin_payment
 
         bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.admin_home -> true
+                R.id.admin_home -> {
+                    startActivity(Intent(applicationContext, AdminOrder::class.java))
+                    finish()
+                    true
+                }
                 R.id.admin_order -> {
                     startActivity(Intent(applicationContext, AdminOrder::class.java))
                     finish()
                     true
                 }
 
-                R.id.admin_payment -> {
-                    startActivity(Intent(applicationContext, AdminPayment::class.java))
-                    finish()
-                    true
-                }
+                R.id.admin_payment -> true
 
                 R.id.admin_donation -> {
                     startActivity(Intent(applicationContext, AdminDonation::class.java))
